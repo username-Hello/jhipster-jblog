774d98fa192c63b9cd74cdb45ff87368ab810795
nikit <nikitos.burak@gmail.com>
2020-10-15 00:12:32 +0300

    initial project version

---
+++ src/main/java/com/jblogapp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jblogapp.config.audit;
---
+++ src/main/java/com/jblogapp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.jblogapp.repository;
+
+import com.jblogapp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
